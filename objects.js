a7ac1c780c663e0ec72a3bccb476e184ee95e765
Ge Wang <wg.wangge1984@gmail.com>
2018-02-22 23:41:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {
   daniel_avery: 'Deone Logic'
   moodymean: 'Lyk U Use 2'
   lone: 'Airglow Fires'
-}
+};
 
 // function updatePlaylist(playlist, nameOfArtist, songTitle) {
 //   playlist[nameOfArtist] = songTitle
